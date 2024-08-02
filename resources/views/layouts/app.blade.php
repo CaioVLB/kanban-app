8e66ad10330739eabab7f5133afbc4ab14bdc477
CaioVLB <pvhcaiovitor10@gmail.com>
2024-08-02 15:25:51 -0400

    Implementação de cards na dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+{{--        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">--}}
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -21,33 +22,5 @@
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @include('layouts.main')
         </div>
-
-        {{-- <div class="flex" id="wrapper" x-data="{ isOpen: true }">
-            <div id="sidebar" class="h-screen overflow-y-auto transition-all duraction-200 bg-white dark:bg-gray-900"
-            :class="isOpen ? 'w-48' : 'w-0'">
-                <div class="w-full h-auto flex justify-center p-2 bg-gray-900 dark:bg-gray-100">
-                    <!-- Logo -->
-                    <div class="shrink-0 flex items-center">
-                        <a href="{{ route('dashboard') }}">
-                            <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
-                        </a>
-                    </div>
-                </div>
-                <ul>
-                    <li>Board</li>
-                    <li>Clientes</li>
-                </ul>
-            </div> --}}
-            {{-- <div class="w-full h-screen overflow-y-auto transition-all duraction-200 bg-gray-100 dark:bg-gray-900">
-                <div class="w-full h-screen overflow-y-auto bg-gray-900 dark:bg-gray-100">
-                    <button @click.prevent="isOpen = ! isOpen">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#000000" viewBox="0 0 256 256"><path d="M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z"></path></svg>
-                    </button>
-                </div>
-                <div class="p-2">
-
-                </div>
-            </div> --}}
-        {{-- </div> --}}
     </body>
 </html>
