f4e8234232be3abed6fb9663f5325748c7d4d759
caio_vitor <pvhcaiovitor10@gmail.com>
2024-09-20 01:31:11 -0400

    Continuação da implementação do quadro kanban

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,7 +16,7 @@
             <!-- Card Prioridade Alta -->
             <div class="bg-red-100 dark:bg-red-600 shadow rounded-lg p-4 flex flex-col items-start">
                 <div class="flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#A30E0E" class="h-6 w-6 mr-2 dark:fill-red-600" viewBox="0 0 256 256">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#A30E0E" class="h-6 w-6 mr-2 dark:fill-red-100" viewBox="0 0 256 256">
                         <path d="M183.89,153.34a57.6,57.6,0,0,1-46.56,46.55A8.75,8.75,0,0,1,136,200a8,8,0,0,1-1.32-15.89c16.57-2.79,30.63-16.85,33.44-33.45a8,8,0,0,1,15.78,2.68ZM216,144a88,88,0,0,1-176,0c0-27.92,11-56.47,32.66-84.85a8,8,0,0,1,11.93-.89l24.12,23.41,22-60.41a8,8,0,0,1,12.63-3.41C165.21,36,216,84.55,216,144Zm-16,0c0-46.09-35.79-85.92-58.21-106.33L119.52,98.74a8,8,0,0,1-13.09,3L80.06,76.16C64.09,99.21,56,122,56,144a72,72,0,0,0,144,0Z"></path>
                     </svg>
                     <h2 class="text-xl font-bold text-red-700 dark:text-red-100">Prioridade Alta</h2>
@@ -43,7 +43,6 @@
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" class="h-6 w-6 text-green-700 dark:text-green-100 mr-2" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25 12 21m0 0-3.75-3.75M12 21V3" />
                     </svg>
-
                     <h2 class="text-xl font-bold text-green-700 dark:text-green-100">Prioridade Baixa</h2>
                 </div>
                 <p class="mt-2 text-green-600 dark:text-green-200">Tarefas que podem ser tratadas depois.</p>
@@ -65,17 +64,29 @@
 
             <div class="xl:w-[20%] lg:w-[25%] md:w-[33.3333%] w-full relative group box-border py-1 px-2">
                 <a href="{{ route('board') }}" class="w-full md:h-[122px] h-[90px] flex flex-col justify-between py-2 px-4 bg-white border border-gray-200 rounded-lg shadow transition-transform duration-300 ease-in-out transform group-hover:scale-105 z-10 hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                    <span class="w-full max-h-[80px] inline-block overflow-scroll overflow-x-hidden scroll-smooth break-words font-bold dark:text-white">Planejamento de Desenvolvimento de Software Para Gestão de Processos</span>
-                    <div class="w-full flex justify-end gap-4">
-                        <div class="flex justify-center items-center gap-1">
+                    <span class="w-full max-h-[80px] inline-block overflow-scroll overflow-y-auto overflow-x-hidden scroll-smooth break-words font-bold dark:text-white">Planejamento de Desenvolvimento de Software Para Gestão de Processos</span>
+                    <div class="w-full flex justify-end gap-2">
+                        <div class="inline-flex justify-center items-center gap-1">
                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#000000" class="dark:fill-white" viewBox="0 0 256 256">
                                 <path d="M230.92,212c-15.23-26.33-38.7-45.21-66.09-54.16a72,72,0,1,0-73.66,0C63.78,166.78,40.31,185.66,25.08,212a8,8,0,1,0,13.85,8c18.84-32.56,52.14-52,89.07-52s70.23,19.44,89.07,52a8,8,0,1,0,13.85-8ZM72,96a56,56,0,1,1,56,56A56.06,56.06,0,0,1,72,96Z"></path>
                             </svg>
-                            <span class="font-normal dark:text-white">10</span>
+                            <span class="font-normal dark:text-white">18</span>
                         </div>
-                        <div class="flex justify-center items-center gap-2">
+                        <div class="inline-flex justify-center items-center gap-1">
                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#FF0000" class="dark:fill-red-600" viewBox="0 0 256 256">
                                 <path d="M183.89,153.34a57.6,57.6,0,0,1-46.56,46.55A8.75,8.75,0,0,1,136,200a8,8,0,0,1-1.32-15.89c16.57-2.79,30.63-16.85,33.44-33.45a8,8,0,0,1,15.78,2.68ZM216,144a88,88,0,0,1-176,0c0-27.92,11-56.47,32.66-84.85a8,8,0,0,1,11.93-.89l24.12,23.41,22-60.41a8,8,0,0,1,12.63-3.41C165.21,36,216,84.55,216,144Zm-16,0c0-46.09-35.79-85.92-58.21-106.33L119.52,98.74a8,8,0,0,1-13.09,3L80.06,76.16C64.09,99.21,56,122,56,144a72,72,0,0,0,144,0Z"></path>
+                            </svg>
+                            <span class="font-normal dark:text-white">6</span>
+                        </div>
+                        <div class="inline-flex justify-center items-center gap-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#000000" class="fill-yellow-700 dark:fill-yellow-100" viewBox="0 0 256 256">
+                                <path d="M236.8,188.09,149.35,36.22h0a24.76,24.76,0,0,0-42.7,0L19.2,188.09a23.51,23.51,0,0,0,0,23.72A24.35,24.35,0,0,0,40.55,224h174.9a24.35,24.35,0,0,0,21.33-12.19A23.51,23.51,0,0,0,236.8,188.09ZM222.93,203.8a8.5,8.5,0,0,1-7.48,4.2H40.55a8.5,8.5,0,0,1-7.48-4.2,7.59,7.59,0,0,1,0-7.72L120.52,44.21a8.75,8.75,0,0,1,15,0l87.45,151.87A7.59,7.59,0,0,1,222.93,203.8ZM120,144V104a8,8,0,0,1,16,0v40a8,8,0,0,1-16,0Zm20,36a12,12,0,1,1-12-12A12,12,0,0,1,140,180Z"></path>
+                            </svg>
+                            <span class="font-normal dark:text-white">6</span>
+                        </div>
+                        <div class="inline-flex justify-center items-center gap-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" class="text-green-700 dark:text-green-100" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25 12 21m0 0-3.75-3.75M12 21V3" />
                             </svg>
                             <span class="font-normal dark:text-white">6</span>
                         </div>
@@ -85,19 +96,25 @@
 
             <div class="xl:w-[20%] lg:w-[25%] md:w-[33.3333%] w-full relative group box-border py-1 px-2">
                 <a href="#" class="w-full md:h-[122px] h-[90px] flex flex-col justify-between py-2 px-4 bg-white border border-gray-200 rounded-lg shadow transition-transform duration-300 ease-in-out transform group-hover:scale-105 z-10 hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                    <span class="w-full max-h-[80px] inline-block overflow-scroll overflow-x-hidden scroll-smooth break-words font-bold dark:text-white">Planejamento do Backlog Para a Sprint</span>
-                    <div class="w-full flex justify-end gap-4">
-                        <div class="flex justify-center items-center gap-1">
+                    <span class="w-full max-h-[80px] inline-block overflow-scroll overflow-y-auto overflow-x-hidden scroll-smooth break-words font-bold dark:text-white">Planejamento do Backlog Para a Sprint</span>
+                    <div class="w-full flex justify-end gap-2">
+                        <div class="inline-flex justify-center items-center gap-1">
                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#000000" class="dark:fill-white" viewBox="0 0 256 256">
                                 <path d="M230.92,212c-15.23-26.33-38.7-45.21-66.09-54.16a72,72,0,1,0-73.66,0C63.78,166.78,40.31,185.66,25.08,212a8,8,0,1,0,13.85,8c18.84-32.56,52.14-52,89.07-52s70.23,19.44,89.07,52a8,8,0,1,0,13.85-8ZM72,96a56,56,0,1,1,56,56A56.06,56.06,0,0,1,72,96Z"></path>
                             </svg>
-                            <span class="font-normal dark:text-white">8</span>
+                            <span class="font-normal dark:text-white">16</span>
                         </div>
-                        <div class="flex justify-center items-center gap-2">
+                        <div class="inline-flex justify-center items-center gap-1">
                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#FF0000" class="dark:fill-red-600" viewBox="0 0 256 256">
                                 <path d="M183.89,153.34a57.6,57.6,0,0,1-46.56,46.55A8.75,8.75,0,0,1,136,200a8,8,0,0,1-1.32-15.89c16.57-2.79,30.63-16.85,33.44-33.45a8,8,0,0,1,15.78,2.68ZM216,144a88,88,0,0,1-176,0c0-27.92,11-56.47,32.66-84.85a8,8,0,0,1,11.93-.89l24.12,23.41,22-60.41a8,8,0,0,1,12.63-3.41C165.21,36,216,84.55,216,144Zm-16,0c0-46.09-35.79-85.92-58.21-106.33L119.52,98.74a8,8,0,0,1-13.09,3L80.06,76.16C64.09,99.21,56,122,56,144a72,72,0,0,0,144,0Z"></path>
                             </svg>
-                            <span class="font-normal dark:text-white">16</span>
+                            <span class="font-normal dark:text-white">8</span>
+                        </div>
+                        <div class="inline-flex justify-center items-center gap-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" class="text-green-700 dark:text-green-100" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25 12 21m0 0-3.75-3.75M12 21V3" />
+                            </svg>
+                            <span class="font-normal dark:text-white">8</span>
                         </div>
                     </div>
                 </a>
@@ -105,19 +122,25 @@
 
             <div class="xl:w-[20%] lg:w-[25%] md:w-[33.3333%] w-full relative group box-border py-1 px-2">
                 <a href="#" class="w-full md:h-[122px] h-[90px] flex flex-col justify-between py-2 px-4 bg-white border border-gray-200 rounded-lg shadow transition-transform duration-300 ease-in-out transform group-hover:scale-105 z-10 hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                    <span class="w-full max-h-[80px] inline-block overflow-scroll overflow-x-hidden scroll-smooth break-words font-bold dark:text-white">Planejamento das Novas Funcionalidades</span>
-                    <div class="w-full flex justify-end gap-4">
-                        <div class="flex justify-center items-center gap-1">
+                    <span class="w-full max-h-[80px] inline-block overflow-scroll overflow-y-auto overflow-x-hidden scroll-smooth break-words font-bold dark:text-white">Planejamento das Novas Funcionalidades</span>
+                    <div class="w-full flex justify-end gap-2">
+                        <div class="inline-flex justify-center items-center gap-1">
                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#000000" class="dark:fill-white" viewBox="0 0 256 256">
                                 <path d="M230.92,212c-15.23-26.33-38.7-45.21-66.09-54.16a72,72,0,1,0-73.66,0C63.78,166.78,40.31,185.66,25.08,212a8,8,0,1,0,13.85,8c18.84-32.56,52.14-52,89.07-52s70.23,19.44,89.07,52a8,8,0,1,0,13.85-8ZM72,96a56,56,0,1,1,56,56A56.06,56.06,0,0,1,72,96Z"></path>
                             </svg>
                             <span class="font-normal dark:text-white">4</span>
                         </div>
-                        <div class="flex justify-center items-center gap-2">
+                        <div class="inline-flex justify-center items-center gap-1">
                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#FF0000" class="dark:fill-red-600" viewBox="0 0 256 256">
                                 <path d="M183.89,153.34a57.6,57.6,0,0,1-46.56,46.55A8.75,8.75,0,0,1,136,200a8,8,0,0,1-1.32-15.89c16.57-2.79,30.63-16.85,33.44-33.45a8,8,0,0,1,15.78,2.68ZM216,144a88,88,0,0,1-176,0c0-27.92,11-56.47,32.66-84.85a8,8,0,0,1,11.93-.89l24.12,23.41,22-60.41a8,8,0,0,1,12.63-3.41C165.21,36,216,84.55,216,144Zm-16,0c0-46.09-35.79-85.92-58.21-106.33L119.52,98.74a8,8,0,0,1-13.09,3L80.06,76.16C64.09,99.21,56,122,56,144a72,72,0,0,0,144,0Z"></path>
                             </svg>
-                            <span class="font-normal dark:text-white">12</span>
+                            <span class="font-normal dark:text-white">2</span>
+                        </div>
+                        <div class="inline-flex justify-center items-center gap-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#000000" class="fill-yellow-700 dark:fill-yellow-100" viewBox="0 0 256 256">
+                                <path d="M236.8,188.09,149.35,36.22h0a24.76,24.76,0,0,0-42.7,0L19.2,188.09a23.51,23.51,0,0,0,0,23.72A24.35,24.35,0,0,0,40.55,224h174.9a24.35,24.35,0,0,0,21.33-12.19A23.51,23.51,0,0,0,236.8,188.09ZM222.93,203.8a8.5,8.5,0,0,1-7.48,4.2H40.55a8.5,8.5,0,0,1-7.48-4.2,7.59,7.59,0,0,1,0-7.72L120.52,44.21a8.75,8.75,0,0,1,15,0l87.45,151.87A7.59,7.59,0,0,1,222.93,203.8ZM120,144V104a8,8,0,0,1,16,0v40a8,8,0,0,1-16,0Zm20,36a12,12,0,1,1-12-12A12,12,0,0,1,140,180Z"></path>
+                            </svg>
+                            <span class="font-normal dark:text-white">2</span>
                         </div>
                     </div>
                 </a>
